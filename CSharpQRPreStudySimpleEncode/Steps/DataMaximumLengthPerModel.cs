3554d893a1ee0756a27863fedff027de3d2c63d5
이정규 <i6020345@live.co.kr>
2018-11-21 19:43:42 +0900

    초기 커밋.

---
+++ CSharpQRPreStudySimpleEncode/Steps/DataMaximumLengthPerModel.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpQRPreStudySimpleEncode.Steps
+{
+    /// <summary>
+    /// 2.2.4 종료 지시자
+    /// 2.2.3에서 나온 결과에 0000을 추가한다. 
+    /// 만약 2.2.3에서 이미 데이터 코드의 길이가 최대에 도달했다면 이 작업은 할 필요가 없다.
+    /// </summary>
+    public class DataMaximumLengthPerModel
+    {
+        /// <summary>
+        /// 표1 의 데이터 테이블
+        /// 버전 1~9의 데이터 코드와 오류 정정 코드의 갯수
+        /// </summary>
+        private DataTable QRMaximumLengthPerModelTable = new DataTable("QRMaximumLengthPerModelTable");
+
+        /// <summary>
+        /// 입력 데이터
+        /// </summary>
+        private string _inputData;
+
+        /// <summary>
+        /// 모드 지시자 값
+        /// 숫자 모드 : 0001
+        /// 알파벳(대문자만) + α 모드: 0010
+        /// 8bit 모드: 0100
+        /// KANJI 모드 : 1000
+        /// </summary>
+        private string _modeIndicator;
+
+        /// <summary>
+        /// 버전
+        /// </summary>
+        private string _version;
+
+        /// <summary>
+        /// 오류 정정 단계
+        /// </summary>
+        private string _errorRestoreLevel;
+
+        /// <summary>
+        /// 생성자
+        /// </summary>
+        /// <param name="inputData">데이터</param>
+        /// <param name="modeIndicator">모드 지시자</param>
+        public DataMaximumLengthPerModel(string inputData, string ver, string restore, string modeIndicator)
+        {
+            init_QRMaximumLengthPerModelTable();
+
+            this._inputData = inputData;
+            this._version = ver;
+            this._errorRestoreLevel = restore;
+            this._modeIndicator = modeIndicator;
+        }
+
+        /// <summary>
+        /// I최대 길이 확인
+        /// </summary>
+        public string GetMaximumLengthPerModel
+        {
+            get { return MaximumLengthPerModel(); }
+        }
+
+        /// <summary>
+        /// I최대 8bit 블락 크기
+        /// </summary>
+        public string GetMax8bitBlock
+        {
+            get { return Max8bitBlock(); }
+        }
+
+        /// <summary>
+        /// 데이터 코드의 최대 크기 반환
+        /// 만약 데이터 코드가 표1의 최대 개수만큼 차지 못했다면 
+        /// "11101100"과 "00010001"을 번갈아 가면서 최대 개수까지 추가해준다.
+        /// </summary>
+        /// <returns></returns>
+        private string Max8bitBlock()
+        {
+            DataRow[] rows;
+            rows = this.QRMaximumLengthPerModelTable.Select(
+                string.Format("Version = '{0}' AND Restore = '{1}'", _version, _errorRestoreLevel)
+                );
+
+            return rows[0]["DataCodeCount"].ToString(); ;
+        }
+
+        /// <summary>
+        /// 최대 길이 확인
+        /// 최대 길이이면 빈값을 반환
+        /// 아니라면 0000을 반환
+        /// 2.2.3에서 나온 결과에 0000을 추가한다. 
+        /// 만약 2.2.3에서 이미 데이터 코드의 길이가 최대에 도달했다면 이 작업은 할 필요가 없다.
+        /// </summary>
+        /// <returns></returns>
+        private string MaximumLengthPerModel()
+        {
+            string resultTailString = string.Empty;
+
+            DataRow[] rows;
+            rows = this.QRMaximumLengthPerModelTable.Select(
+                string.Format("Version = '{0}' AND Restore = '{1}'", _version, _errorRestoreLevel)
+                );
+
+            string _fieldName = string.Empty;
+            //숫자 모드 : 0001
+            //알파벳(대문자만) + α 모드: 0010
+            //8bit 모드: 0100
+            //KANJI 모드 : 1000
+            switch (_modeIndicator)
+            {
+                case "0001":
+                    _fieldName = "Numberic";
+                    break;
+                case "0010":
+                    _fieldName = "Alphanumeric";
+                    break;
+                case "0100":
+                    _fieldName = "Eightbit";
+                    break;
+                case "1000":
+                    _fieldName = "Alphanumeric"; //KANJI 모드는 임시로 이거로..
+                    break;
+            }
+
+            int _maximumLength = 0;
+            _maximumLength = int.Parse(rows[0][_fieldName].ToString());
+
+            //만약 2.2.3에서 이미 데이터 코드의 길이가 최대에 도달했다면 이 작업은 할 필요가 없다.
+            if (_inputData.Length < _maximumLength)
+            {
+                resultTailString = "0000";
+            }
+
+            return resultTailString;
+        } // end MaximumLengthPerModel
+
+        #region 표1. 버전 1~9의 데이터 코드와 오류 정정 코드의 갯수
+        /// <summary>
+        /// 표1. 버전 1~9의 데이터 코드와 오류 정정 코드의 갯수
+        /// </summary>
+        private void init_QRMaximumLengthPerModelTable()
+        {
+            //대소문자 사용 해야하나???
+            this.QRMaximumLengthPerModelTable.CaseSensitive = false;
+
+            //항목
+            //버전/오류 정정 단계/데이터 코드의 갯수/오류 정정 코드의 갯수/숫자/알파벳+α/8bit
+            // numeric / alphanumeric / eightbit
+            this.QRMaximumLengthPerModelTable.Columns.Add("Version", typeof(string));
+            this.QRMaximumLengthPerModelTable.Columns.Add("Restore", typeof(string));
+            this.QRMaximumLengthPerModelTable.Columns.Add("DataCodeCount", typeof(string));
+            this.QRMaximumLengthPerModelTable.Columns.Add("ErrorRestoreCount", typeof(string));
+            this.QRMaximumLengthPerModelTable.Columns.Add("Numberic", typeof(string));
+            this.QRMaximumLengthPerModelTable.Columns.Add("Alphanumeric", typeof(string));
+            this.QRMaximumLengthPerModelTable.Columns.Add("Eightbit", typeof(string));
+
+            //data
+            //this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "A", "B", "C", "D", "E", "F", "G" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "1", "L", "19", "7", "41", "25", "17" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "1", "M", "16", "10", "34", "20", "14" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "1", "Q", "13", "13", "27", "16", "11" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "1", "H", "9", "17", "17", "10", "7" });
+
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "2", "L", "34", "10", "77", "47", "32" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "2", "M", "28", "16", "63", "38", "26" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "2", "Q", "22", "22", "48", "29", "20" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "2", "H", "16", "28", "34", "20", "14" });
+
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "3", "L", "55", "15", "127", "77", "53" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "3", "M", "44", "26", "101", "61", "42" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "3", "Q", "34", "36", "77", "47", "32" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "3", "H", "26", "44", "58", "35", "24" });
+
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "4", "L", "80", "20", "187", "114", "78" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "4", "M", "64", "36", "149", "90", "62" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "4", "Q", "48", "52", "111", "67", "46" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "4", "H", "36", "64", "82", "50", "34" });
+
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "5", "L", "108", "26", "255", "154", "106" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "5", "M", "86", "48", "202", "122", "84" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "5", "Q", "62", "72", "144", "87", "60" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "5", "H", "46", "88", "106", "64", "44" });
+
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "6", "L", "136", "36", "322", "195", "134" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "6", "M", "108", "64", "255", "154", "106" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "6", "Q", "76", "96", "175", "108", "74" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "6", "H", "60", "112", "139", "84", "58" });
+
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "7", "L", "156", "40", "370", "224", "154" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "7", "M", "124", "72", "293", "178", "122" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "7", "Q", "88", "108", "207", "125", "86" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "7", "H", "66", "130", "154", "93", "64" });
+
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "8", "L", "194", "48", "461", "279", "192" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "8", "M", "154", "88", "365", "221", "152" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "8", "Q", "110", "132", "259", "157", "108" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "8", "H", "86", "156", "202", "122", "84" });
+
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "9", "L", "232", "60", "552", "335", "230" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "9", "M", "182", "110", "432", "262", "180" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "9", "Q", "132", "160", "312", "189", "130" });
+            this.QRMaximumLengthPerModelTable.Rows.Add(new object[] { "9", "H", "100", "192", "235", "143", "98" });
+
+            //... 여기까지만 일단 입력 ㄷㄷ
+
+        } // end init_QRMaximumLengthPerModelTable
+        #endregion
+    } // end class DataMaximumLengthPerModel
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,169 @@
+﻿using CSharpQRPreStudySimpleEncode.Steps;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            //2.2 데이터 코드로 인코딩
+            string iVersion = "1"; //버전
+            string iErrorRestoreLevel = "H"; // 오류 정정 단계
+            string iStr = "ABCDE123"; //입력문자를 이걸로 가정 버전 1, 오류 정정 단계 H(이하 1-H라 한다.)
+
+            //2.2.1 모드 지시자
+            //숫자 모드 : 0001
+            //알파벳(대문자만) + α 모드: 0010
+            //8bit 모드: 0100
+            //KANJI 모드 : 1000
+
+            string ModeIndicator = "0010";  //ABCDE123 는 알파벳+a => 0010
+
+            Console.WriteLine("ModeIndicator : " + ModeIndicator);
+            sb.Append(ModeIndicator);
+
+            //2.2.2 데이터 갯수 지시자
+            //숫자: 10bit
+            //알파벳(대문자만) + α : 9bit
+            //8bit: 8bit
+            //KANJI : 8bit
+
+            int x = 8;
+            string s = Convert.ToString(x, 2);
+
+            //ABCDE123 은 8문자임으로 8을 9bit(알파벳 + α) 바이너리 데이터로 인코딩 해야함
+
+            int[] bits = s.PadLeft(9, '0')
+                .Select(c => int.Parse(c.ToString()))
+                .ToArray();
+
+            foreach (int item in bits)
+            {
+                //EncodingData.Add(item.ToString());
+                Console.Write(item);
+                sb.Append(item.ToString());
+            }
+            Console.WriteLine("");
+
+            //2.2.3 데이터 인코딩
+            //숫자 모드에서는, 데이터는 각각 3자리의 숫자들로 나뉜다.
+            //예로 "123456"은 "123"과 "456"으로 나뉜다. 순서는 바뀌지 않는다.
+            //또한 각각의 나뉜 데이터는 10bit 바이너리 데이터로 인코딩된다.
+
+            //만약 나뉜 데이터에 숫자가 1개 또는 2개라면, 각각 4bit와 7bit의 바이너리 데이터로 인코딩해야 한다.
+            //예로 "9876"은 "987"과 "6"으로 나뉘는데, "987"은 10bit, "6"은 4bit 바이너리 데이터로 인코딩한다.
+            //이것의 결과는 "1111011011 0110"이 된다.
+
+            //알파벳(대문자만) + α 모드에서는, 표2에 의해 모든 문자가 특정한 숫자로 바뀐다.
+            //그 다음 모든 숫자들을 2개씩 나눈다. 2개 중 첫 번째 값에 45를 
+            //곱하고 두 번째 값을 더한 결과를 11bit 바이너리 데이터로 인코딩하면 된다.
+            //만약 이 결과가 10보다 작을 때에는 6bit 바이너리 데이터로 인코딩해야 한다.
+            DataEncodingModeAlphabatPlusAlpha demap = new DataEncodingModeAlphabatPlusAlpha(iStr);
+            Console.WriteLine("DataEncodingModeAlphabatPlusAlpha : " + demap.GetEncodingData);
+            sb.Append(demap.GetEncodingData);
+
+            //2.2.4 종료 지시자
+            //2.2.3에서 나온 결과에 0000을 추가한다. 
+            //만약 2.2.3에서 이미 데이터 코드의 길이가 최대에 도달했다면 이 작업은 할 필요가 없다.
+            DataMaximumLengthPerModel dmlp = new DataMaximumLengthPerModel(iStr, iVersion, iErrorRestoreLevel, ModeIndicator);
+            //Console.WriteLine(dmlp.GetMaximumLengthPerModel);
+            string EndIndicator = dmlp.GetMaximumLengthPerModel;
+
+            Console.WriteLine("DataMaximumLengthPerModel : " + EndIndicator);
+            sb.Append(EndIndicator);
+
+
+            //2.2.5 데이터 코드로 인코딩
+            //2.2.4에서 나온 결과를 8bit 단위로 나눈다.
+            //test print
+
+            for (int i = 0; i < sb.ToString().Length; i++)
+            {
+                Console.Write(sb[i].ToString());
+                if (((i + 1) % 8) == 0)
+                {
+                    Console.Write(" ");
+                }
+            }
+
+            //만약 마지막 8bit가 모두 차지 않았다면 남은 bit를 모두 0으로 채운다.
+            //Console.WriteLine("\n남은 bit start");
+            //Console.WriteLine(8 - (sb.ToString().Length % 8));
+            int curLength = (8 - (sb.ToString().Length % 8));
+            for (int i = 0; i < curLength; i++)
+            {
+                //Console.Write(" 0" + i + "번째");
+                sb.Append("0");
+            }
+            //sb.Append("000");
+            //Console.WriteLine("\n남은 bit end");
+
+            //Console.WriteLine("\nX : " + (sb.ToString().Length % 8));
+
+            //EndIndicator 까지 더해서 남은 비트를 체운상태의 문자열이 지금까지 sb에 담겨져있다
+            //여기서 8bit로 변환하고 블럭수를 해당 버전과 정정단계 코드값으로 최대 블럭수를 받아와서 비교한다
+
+            //버전과 정정코드별 최대 데이터 코드 갯수(8bit block의 수)
+            int max8bitBlockSize = int.Parse(dmlp.GetMax8bitBlock);
+
+            //int a = sb.ToString().Length;
+            //int b = 8;
+            //double d = a / b;
+
+            Console.WriteLine("전체 block수 : " + max8bitBlockSize);
+            //Console.WriteLine("몫 : " + System.Math.Truncate(d));
+            //Console.WriteLine("나머지 : " + (a & b));
+
+            Console.WriteLine("현재 block수 : " + sb.ToString().Length / 8);
+
+            //만약 데이터 코드가 표1의 최대 개수만큼 차지 못했다면 
+            //"11101100"과 "00010001"을 번갈아 가면서 최대 개수까지 추가해준다.
+
+            //max8bitBlockSize = 20;
+            for (int i = 0; i < max8bitBlockSize - (sb.ToString().Length / 8); i++)
+            {
+                if ((i % 2) == 0)
+                {
+                    Console.WriteLine("짝수 더한다" + i);
+                    sb.Append("11101100");
+                }
+                else
+                {
+                    Console.WriteLine("홀수 더한다" + i);
+                    sb.Append("00010001");
+                }
+            } // end for
+
+            Console.WriteLine("\n-------------------------");
+            Console.WriteLine(sb.ToString());
+
+            //2진수를 10진수로 변환해서 검수
+            Console.WriteLine(Convert.ToByte("00100000", 2));
+
+            string _loopString = string.Empty;
+
+            Console.WriteLine("\n----------------------result");
+            for (int i = 0; i < sb.ToString().Length; i++)
+            {
+                _loopString += sb[i].ToString();
+
+                if (((i + 1) % 8) == 0)
+                {
+                    Console.WriteLine(
+                        string.Format("{0} -> 10 decimal : {1}"
+                        , _loopString
+                        , Convert.ToByte(_loopString, 2))
+                        );
+                    _loopString = string.Empty;
+                }
+            } // end for
+        }
+    }
+}
